ae4e2c9d6f445d96490ffa6cc4b1cf2a03f9f19e
sujatha <devinenisujatha1995@gmail.com>
2019-10-10 16:01:35 -0400

    latest

---
+++ src/main/java/sujatha/App.java
@@ -0,0 +1,13 @@
+package sujatha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
